c8817d8224383c879fab3713e87615bbc8ec5123
hawi088 <hawi.ugr-4097-16@aau.edu.et>
2026-09-12 10:44:23 +0300

    feat: build XOR model with PyTorch nn.Linear layers

--- pytorch/pytorch_neural_network.py
+++ pytorch/pytorch_neural_network.py
@@ -17,3 +17,15 @@ y = torch.tensor([
     [0.0]
 ])
 
+class XORModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(2,2)
+        self.layer2 = nn.Linear(2,2)
+
+    def forward(self,x):
+        Z1 = self.layer1(x)
+        A1 = torch.relu(Z1)
+        Z2 = self.layer2(A1)
+        return Z2
+model = XORModel()
