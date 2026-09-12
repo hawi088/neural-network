08bec2cb799162b6fb25c29cc56079719e2211cf
hawi088 <hawi.ugr-4097-16@aau.edu.et>
2026-09-12 11:00:13 +0300

    fix: fixed label diamention for crossEntropy compatablity

--- pytorch/pytorch_neural_network.py
+++ pytorch/pytorch_neural_network.py
@@ -1,3 +1,4 @@
+print("Script started")
 import torch
 import torch.nn as nn
 
@@ -10,12 +11,7 @@ X = torch.tensor([
 ])
 
 #create tensor for labels of XOR
-y = torch.tensor([
-    [0.0],
-    [1.0],
-    [1.0],
-    [0.0]
-])
+y = torch.tensor([0,1,1,0]) #labels for XOR
 
 class XORModel(nn.Module):
     def __init__(self):
@@ -29,3 +25,10 @@ class XORModel(nn.Module):
         Z2 = self.layer2(A1)
         return Z2
 model = XORModel()
+logits = model(X)
+print(logits) #random initialization of weights and biases, so output will be different each time
+
+#calculate the loss
+loss_fn = nn.CrossEntropyLoss()
+loss = loss_fn(logits, y) #convert y to long type for CrossEntropyLoss
+print(loss)
